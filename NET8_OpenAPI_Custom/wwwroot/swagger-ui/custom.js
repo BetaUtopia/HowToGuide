f476cf9064e63abe196740b76d08f94861b33e19
BetaUtopia <BetaUtopia@users.noreply.github.com>
2024-08-01 19:29:45 -0500

    Update to fix logo on latest version
    Update to fix logo on latest version

--- NET8_OpenAPI_Custom/wwwroot/swagger-ui/custom.js
+++ NET8_OpenAPI_Custom/wwwroot/swagger-ui/custom.js
@@ -1,8 +1,28 @@
-var links = document.getElementsByTagName("link");
-for (var linkIndex = 0; linkIndex < links.length; linkIndex++) {
-	var link = links[linkIndex];
-	if (link) {
-		if (link.rel == 'icon')
-			link.href = '/docs/favicon-light.ico';
-	}
-}
+document.addEventListener('DOMContentLoaded', function () {
+    // Replace favicon
+    var links = document.getElementsByTagName("link");
+    for (var linkIndex = 0; linkIndex < links.length; linkIndex++) {
+        var link = links[linkIndex];
+        if (link && link.rel === 'icon') {
+            link.href = '../docs/favicon-light.ico';
+        }
+    }
+
+    // Replace SVG with image
+    function replaceSvgWithImage() {
+        var svgElement = document.querySelector('.swagger-ui .topbar-wrapper .link svg');
+        if (svgElement) {
+            var imgElement = document.createElement('img');
+            imgElement.src = '../docs/favicon-light.png';
+            imgElement.alt = 'Logo';
+            imgElement.style.height = '32px';
+            imgElement.style.width = 'auto';
+
+            var parent = svgElement.parentNode;
+            parent.replaceChild(imgElement, svgElement);
+        } else {
+            setTimeout(replaceSvgWithImage, 100);
+        }
+    }
+    replaceSvgWithImage();
+});
